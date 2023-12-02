4906f4e564eb60d4405156e537be19681f928edf
Oliver Beine <OliverBeine@gmx.net>
2023-12-02 12:10:43 +0100

    fixed da bug in indention and added a missing function

--- sumNumbersInStrings.py
+++ sumNumbersInStrings.py
@@ -4,16 +4,19 @@ numbersString = ["zero", "one", "two", "three", "four", "five", "six", "seven", 
 numberDigit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 Numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
+#Definitions
+def indexes(iterable, obj):
+	return (index for index, elem in enumerate(iterable) if elem == obj)
+
 #Read lines of file
 file_path = 'AOC011223input.txt'
 with open(file_path, 'r', encoding='utf-8') as file:
 	Lines = file.readlines()
 lineCount = 0
 for line in Lines:
-    	lineCount += 1
-    	#print("Line{}: {}".format(count, line.strip()))
-    	numberCount = 0
+	lineCount += 1 #print("Line{}: {}".format(count, line.strip()))
+	numberCount = 0
 	for number in Numbers:
-		indexes = indexes(line, number)
-      		print("Number "+number+ " appears in line "+line+" at position(s) "+list(indexes))
+		idxs = indexes(line, number)
+		print("Number "+number+ " appears in line "+line+" at position(s) "+str(list(idxs)))
 		lineCount += 1
