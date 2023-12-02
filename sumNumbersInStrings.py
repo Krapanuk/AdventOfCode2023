d8cea76fb6031f50f174c6564daa0db6534e02d5
Krapanuk <46630690+Krapanuk@users.noreply.github.com>
2023-12-02 11:54:33 +0100

    Update sumNumbersInStrings.py changed input filename

--- sumNumbersInStrings.py
+++ sumNumbersInStrings.py
@@ -5,7 +5,7 @@ numberDigit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 Numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 #Read lines of file
-file_path = 'AOC_011223_StringData.txt'
+file_path = 'AOC011223input.txt'
 with open(file_path, 'r', encoding='utf-8') as file:
 	Lines = file.readlines()
 lineCount = 0
