f4ab71748606e1644186d44c737f2f270298cdcd
Krapanuk <46630690+Krapanuk@users.noreply.github.com>
2023-12-02 11:52:31 +0100

    Update sumNumbersInStrings.py adding output

--- sumNumbersInStrings.py
+++ sumNumbersInStrings.py
@@ -5,13 +5,15 @@ numberDigit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 Numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 #Read lines of file
-file_path = 'StringData.txt'
+file_path = 'AOC_011223_StringData.txt'
 with open(file_path, 'r', encoding='utf-8') as file:
-    Lines = file.readlines()
-count = 0
+	Lines = file.readlines()
+lineCount = 0
 for line in Lines:
-    count += 1
-    print("Line{}: {}".format(count, line.strip()))
-    for number in Numbers:
-			indexes = indexes(line, number)
-      print(list(indexes))
+    	lineCount += 1
+    	#print("Line{}: {}".format(count, line.strip()))
+    	numberCount = 0
+	for number in Numbers:
+		indexes = indexes(line, number)
+      		print("Number "+number+ " appears in line "+line+" at position(s) "+list(indexes))
+		lineCount += 1
