e5310e48a66e7db65ef848339e5ed938cb7f5fe2
Krapanuk <46630690+Krapanuk@users.noreply.github.com>
2023-12-02 12:24:47 +0100

    sumNumbersInStrings.py replaced all string numbers in each line by one digit numbers

--- sumNumbersInStrings.py
+++ sumNumbersInStrings.py
@@ -1,7 +1,7 @@
 
 #Arrays
-numbersString = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-numberDigit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+NumbersString = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+NumbersDigit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 Numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 #Definitions
@@ -15,8 +15,12 @@ with open(file_path, 'r', encoding='utf-8') as file:
 lineCount = 0
 for line in Lines:
 	lineCount += 1 #print("Line{}: {}".format(count, line.strip()))
+	numberStringCount = 0
+	for numberString in NumbersString:
+		  new_string = line.replace(numberString, NumbersDigit[numberStringCount])
+				numberStringCount += 1
 	numberCount = 0
-	for number in Numbers:
-		idxs = indexes(line, number)
-		print("Number "+number+ " appears in line "+line+" at position(s) "+str(list(idxs)))
+	for number in NumbersDigit:
+		idxs = indexes(new_string, number)
+		print("Number "+number+ " appears in line "+new_string+" at position(s) "+str(list(idxs)))
 		lineCount += 1
