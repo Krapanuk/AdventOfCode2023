91cce0989cc5e3fe12c84d0d352774533e3b4a30
Krapanuk <46630690+Krapanuk@users.noreply.github.com>
2023-12-09 10:39:59 +0100

    Update AOCDay9-1.py

--- AOCDay9-1.py
+++ AOCDay9-1.py
@@ -3,5 +3,9 @@
 # 2) To the array for each line append another array with alle differences 
 #    a. for all lines within the array: for l in range (0, len(ArrayOfAllLines)):
 #       for all values within the first to the last sub-Array: for i in range (1, len(ArrayOfAllLines[l])):
-#       calculate des difference: difference = int(ArrayOfAllLines[l][i-1])
+#       calculate des difference: 
+#          1stValue = int(ArrayOfAllLines[l][i-1])
+#          2ndValue = int(ArrayOfAllLines[l][i])
+#          if 1stValue > 2ndValue: difference = 1stValue - 2ndValue
+#          else: difference = 2ndValue - 1stValue
 #    b. ArrayOfAllLines[LineNr].append[Array of diffs]
