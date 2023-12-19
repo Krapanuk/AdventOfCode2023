d61b57bc9542c8be288fb2b66417e33349fed364
Oliver Beine <OliverBeine@gmx.net>
2023-12-19 09:35:00 +0100

    changed day 11-1

--- AOCDay111.py
+++ AOCDay111.py
@@ -22,7 +22,7 @@ def readString(Lines):
 		SpaceWithoutExpansion.append(line)
 	return SpaceWithoutExpansion
 
-def expand(SpaceWithoutExpansion):
+def expand1(SpaceWithoutExpansion):
 	Expanded = []
 	lineCount = 0
 	for line in SpaceWithoutExpansion:
@@ -33,6 +33,7 @@ def expand(SpaceWithoutExpansion):
 			Expanded.append(line)
 	containsHash = False
 	for charPos in range(0, len(SpaceWithoutExpansion[0])-1):
+		LineArray = []
 		for lineNr in range (0,len(SpaceWithoutExpansion)):
 			if SpaceWithoutExpansion[lineNr][charPos] == "#":
 				print("LineNr["+str(lineNr)+"] CharPos["+str(charPos)+"]" +SpaceWithoutExpansion[lineNr][charPos])
@@ -45,6 +46,59 @@ def expand(SpaceWithoutExpansion):
 				#Expanded[lineNr][charPos] = ".."
 	writeToFile(Expanded,"AOC111223Output.txt")
 	return Expanded
+
+
+def addLinesAndColumns(PArray):
+	addedArray = []
+	for n in range(0, len(PArray)): # Add new empty line at every second line
+		Line = []
+		EmptyLine = []
+		for m in range(0, len(PArray[n])):
+			Line.append(PArray[n][m])
+			Line.append('*')
+			EmptyLine.append('*')
+			EmptyLine.append('*')
+		addedArray.append(Line)
+		addedArray.append(EmptyLine)	
+	return connectPipes(addedArray)
+
+def expand(SpaceWithoutExpansion):
+	AddedArray = []
+	for line in range(0, len(SpaceWithoutExpansion)):
+		Line = []
+		for char in range(0, len(SpaceWithoutExpansion[line])):
+			Line.append(SpaceWithoutExpansion[line][char])
+			if hashInColumn == False:
+				Line.append(".")
+		AddedArray.append(Line)
+	for charPos in range(0, len(SpaceWithoutExpansion[0])-1):
+		LineArray = []
+		for lineNr in range (0,len(SpaceWithoutExpansion)):
+			if SpaceWithoutExpansion[lineNr][charPos] == "#":
+				print("LineNr["+str(lineNr)+"] CharPos["+str(charPos)+"]" +SpaceWithoutExpansion[lineNr][charPos])
+				containsHash = True
+			else: containsHash = False
+		if containsHash == False:
+			print("Column ["+str(charPos)+"] without #")
+			for lineNr in range (0,lineCount):
+				Expanded[lineNr] = Expanded[lineNr][:charPos] +"."+ Expanded[lineNr][charPos:]
+				#Expanded[lineNr][charPos] = ".."
+	writeToFile(Expanded,"AOC111223Output.txt")
+	return Expanded
+
+def hashInLine(SpaceWithoutExpansion, line):
+	hInLine = False
+	for char in range(0, len(SpaceWithoutExpansion[line])):
+		if SpaceWithoutExpansion[line][char] == "#":
+			hInLine = True
+	return hInLine
+
+def hashInColumn(SpaceWithoutExpansion, char):
+	hInColumn = False
+	for line in range(0, len(SpaceWithoutExpansion)):
+		if SpaceWithoutExpansion[line][char] == "#":
+			hInColumn = True
+	return hInColumn
 
 def writeToFile(PArray, filename):
 	with open(filename, "w") as txt_file:
